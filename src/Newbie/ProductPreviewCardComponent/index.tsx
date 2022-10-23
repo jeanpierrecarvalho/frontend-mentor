2a2502adbbb23979bd8834a0365eaa6fab02011d
Jean-Pierre Carvalho <hello@jeanpierrecarvalho.pt>
2022-10-23 00:15:14 +0000

    Added React and Typescript

---
+++ src/Newbie/ProductPreviewCardComponent/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './index.css';
+
+function ProductPreviewCardComponent() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+         asdasdasd
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default ProductPreviewCardComponent;
